cd507130dad7c6ae9fecf92c71532a023f26b38d
Matt Guindin <matt@guindin.com>
2013-08-15 00:23:48 -0400

    adding inital maven pom and initial java code for cryptochat

---
+++ src/main/java/com/mguindin/cryptochat/App.java
@@ -0,0 +1,11 @@
+package com.mguindin.cryptochat;
+
+/**
+ * author: matt <matt@guindin.com>
+ */
+public class App {
+
+    public static void main(String[] args) {
+        //do nothing for now
+    }
+}
---
+++ src/test/java/com/mguindin/cryptochat/AppTest.java
@@ -0,0 +1,16 @@
+package com.mguindin.cryptochat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * author: matt <matt@guindin.com>
+ */
+public class AppTest {
+
+    @Test
+    public void testSample() {
+        assertTrue(2 == 2);
+    }
+}
