b3f94f0e75262b42920efc4a4f3c43a36bd7529c
Matt Guindin <matt@guindin.com>
2013-08-19 22:14:44 -0400

    adding bouncycastle to POM and marking version 0.1.0 through initial dev

--- src/main/java/com/mguindin/cryptochat/App.java
+++ src/main/java/com/mguindin/cryptochat/App.java
@@ -6,6 +6,13 @@ package com.mguindin.cryptochat;
 public class App {
 
     public static void main(String[] args) {
-        //do nothing for now
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar cryptochat.jar <IP> <public key file>");
+            System.exit(-1);
+        }
+        // create Contact to communicate with
+        Contact contact = new Contact(args[0], args[1]);
+
+        //next we need to initialize a server to communicate
     }
 }
---
+++ src/main/java/com/mguindin/cryptochat/Contact.java
@@ -0,0 +1,15 @@
+package com.mguindin.cryptochat;
+
+import java.net.Inet4Address;
+import java.security.KeyStore;
+
+/**
+ * author: matt <matt@sailthru.com>
+ */
+public class Contact {
+    private Inet4Address address;
+    private KeyStore pubKey;
+
+    public Contact(String IP, String fileLocation) {
+    }
+}
